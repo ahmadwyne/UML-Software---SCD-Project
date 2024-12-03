6af699c2e6a8a35c39052035bbac2ab69e482170
ahmadwyne <ahmad.wyne748@gmail.com>
2024-12-03 21:52:52 +0500

    Mouse Import

--- UML-SCD/src/main/java/com/example/umlscd/ClassDiagramUI.java
+++ UML-SCD/src/main/java/com/example/umlscd/ClassDiagramUI.java
@@ -13,6 +13,7 @@ import javafx.stage.FileChooser;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.awt.event.MouseEvent;
 import java.io.File;
 
 import java.io.IOException;
